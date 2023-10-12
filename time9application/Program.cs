5e944a403bfe63f3b2686c798a927276fe0c6378
poteter <eith2312@gmail.com>
2023-10-12 11:28:25 +0200

    hour 2 of first lecture, added addstudents and add education to database

--- time9application/Program.cs
+++ time9application/Program.cs
@@ -1,7 +1,42 @@
-﻿namespace time9application {
+﻿using time9application.DataAccess.SqLite;
+using time9application.Entities;
+
+namespace time9application {
 	internal class Program {
 		static void Main() {
-			
+			int educationId = AddEducation();
+			AddStudents(educationId);
+			WriteEducationsToTerminal();
+		}
+
+		static void AddStudents(int id) {
+			List<Student> students = new() {
+			new() { Name = "wololo", EducationId = id },
+			new() { Name = "woah", EducationId = id },
+			new() { Name = "kaffe", EducationId = id },
+			};
+
+			using SchoolDbContext db = new();
+			db.AddRange(students);
+			db.SaveChanges();
+		}
+
+
+		static int AddEducation() {
+			Education education = new() { Name = "Data Science" };
+			using SchoolDbContext db = new();
+			db.Education.Add(education);
+			db.SaveChanges();
+			return education.Id;
+		}
+
+		static void WriteEducationsToTerminal() {
+			using SchoolDbContext db = new(); 
+
+			IList<Education> educations = db.Education.ToList();
+			foreach (var education in educations) {
+                Console.WriteLine(education);
+            }
 		}
 	}
 }
